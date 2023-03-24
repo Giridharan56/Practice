396e8e5f658119a6e2b979333b4f5d417bbffba2
Giridharan56 <giridharanm31@gmail.com>
2023-03-24 10:59:48 +0530

    Next One

---
+++ src/test/java/org/test/Ajii.java
@@ -0,0 +1,23 @@
+package org.test;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.*;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Ajii  {
+		public static void main(String[] args) throws InterruptedException {
+			WebDriverManager.chromedriver().setup();
+			WebDriver d = new ChromeDriver();
+			d.get("https://www.instagram.com/");
+			//d.navigate().refresh();
+			Thread.sleep(3000);
+			
+			d.quit();
+			
+		}
+
+	}
+
+
+
